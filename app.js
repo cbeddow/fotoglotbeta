93fc098c4158f5bd804452b288568eb865f0bdd1
Christopher Beddow <christopher.beddow@gmail.com>
2023-08-22 23:02:31 +0200

    fix: refactor again

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // Get the camera button element
 const cameraButton = document.getElementById('camera-button');
-const backButton = document.getElementById('back-button');
-const languageDropdown = document.getElementById('language');
 
-let selectedLanguage = 'en';
+navigator.mediaDevices.getUserMedia({
+  video: { facingMode: { exact: "environment" } }
+}).then(function(stream) {
+  var video = document.querySelector('#video');
+  video.srcObject = stream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+}).catch(function(err) {
+  console.log("Error: " + err);
+});
 
 // Add a click event listener to the camera button
 cameraButton.addEventListener('click', function() {
@@ -11,104 +19,112 @@ cameraButton.addEventListener('click', function() {
   const video = document.getElementById('video');
   const canvas = document.getElementById('canvas');
   const stillframe = document.getElementById('stillframe');
-  
-  // Hide the video and canvas elements, show the stillframe element
+  const labelElement = document.getElementById('label');
+  const languageSelect = document.getElementById('language-select');
+
+  // Hide the video element, show the stillframe element
   video.style.display = 'none';
-  canvas.style.display = 'none';
   stillframe.style.display = 'block';
-  
-  // Get the captured image element
-  const capturedImage = document.getElementById('captured-image');
-  
-  // Set the source of the image element to the stillframe
-  capturedImage.src = stillframe.src;
-  
+
+  // Capture the image from the video stream
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+  stillframe.setAttribute('src', canvas.toDataURL('image/png'));
+
   // Classify the captured image
-  classifyImage(capturedImage).then(label => {
-    // Translate the label to the selected language
-    translateLabelToLanguage(label, selectedLanguage).then(translatedLabel => {
-      // Display the predicted label over the stillframe
-      const labelElement = document.getElementById('label');
-      labelElement.innerHTML = translatedLabel;
+  classifyImage(stillframe).then(label => {
+    const selectedLanguage = languageSelect.value;
+    
+    if (selectedLanguage !== 'en') {
+      // Translate the label to the selected language
+      translateText(label, selectedLanguage).then(translation => {
+        // Display the translated label over the stillframe
+        labelElement.innerHTML = translation;
+        labelElement.style.display = 'block';
+      });
+    } else {
+      // Display the label without translation
+      labelElement.innerHTML = label;
       labelElement.style.display = 'block';
-    });
+    }
   });
-  
+
   // Show the "Back to Camera" button
-  backButton.style.display = 'inline-block';
-  // Hide the camera button
-  cameraButton.style.display = 'none';
+  backToCameraButton.style.display = 'inline-block';
 });
 
-// Add a change event listener to the language dropdown
-languageDropdown.addEventListener('change', function() {
-  selectedLanguage = this.value;
-});
+// Get the back button element
+const backToCameraButton = document.getElementById('back-button');
 
 // Add a click event listener to the back button
-backButton.addEventListener('click', function() {
+backToCameraButton.addEventListener('click', function() {
   // Get the video, canvas, and stillframe elements
   const video = document.getElementById('video');
-  const canvas = document.getElementById('canvas');
   const stillframe = document.getElementById('stillframe');
-  
-  // Hide the stillframe element, show the video and canvas elements
-  video.style.display = 'block';
-  canvas.style.display = 'block';
+  const labelElement = document.getElementById('label');
+  const languageSelect = document.getElementById('language-select');
+
+  // Hide the stillframe element, show the video element
   stillframe.style.display = 'none';
-  
+  video.style.display = 'block';
+
+  // Hide the label and reset the selected language
+  labelElement.style.display = 'none';
+  languageSelect.value = 'en';
+
   // Hide the "Back to Camera" button
-  backButton.style.display = 'none';
-  // Show the camera button
-  cameraButton.style.display = 'inline-block';
+  backToCameraButton.style.display = 'none';
 });
 
-async function classifyImage(image) {
-  // Load the MobileNet model
-  const model = await mobilenet.load();
-  
-  // Convert the image to a tensor
-  const tensor = tf.browser.fromPixels(image);
-  
-  // Resize the tensor to 224x224 and normalize its values
-  const resized = tf.image.resizeBilinear(tensor, [224, 224]).div(255);
-  
-  // Make a prediction with the model
-  const prediction = await model.classify(resized);
-  
-  // Get the top predicted label
-  const topLabel = prediction[0].className;
-  
-  // Clean up
-  tensor.dispose();
-  resized.dispose();
-  model.dispose();
-  
-  // Return the label
-  return topLabel;
+
+function classifyImage(image) {
+  return new Promise((resolve, reject) => {
+    // Load the InceptionV3 model
+    tf.loadGraphModel('https://tfhub.dev/google/tfjs-model/imagenet/inception_v3/classification/5').then(model => {
+      // Convert the image to a tensor
+      const tensor = tf.browser.fromPixels(image);
+
+      // Resize the tensor to 299x299 and normalize its values
+      const resized = tf.image.resizeBilinear(tensor, [299, 299]).div(255);
+
+      // Make a prediction with the model
+      model.predict(resized.expandDims()).data().then(prediction => {
+        // Get the top predicted label
+        const topLabelIndex = Array.from(prediction).indexOf(Math.max(...prediction));
+        const labels = fetch('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_1.0_224_frozen.json')
+          .then(res => res.json())
+          .then(data => {
+            const label = data[topLabelIndex];
+            resolve(label);
+          })
+          .catch(error => reject(error));
+      });
+    });
+  });
 }
 
-async function translateLabelToLanguage(label, language) {
-  // Make a request to OpenAI API for translation
-  const response = await fetch('https://api.openai.com/v1/translate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer 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'
-    },
-    body: JSON.stringify({
-      text: label,
-      model: `text-davinci-003`,
-      target_lang: language
-    })
+function translateText(text, language) {
+  return new Promise((resolve, reject) => {
+    // Make a request to OpenAI for translation
+    fetch('https://api.openai.com/v1/engines/translation-codex/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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'
+      },
+      body: JSON.stringify({
+        prompt: `Translate the following English text to ${language}: ${text}`,
+        max_tokens: 16,
+        temperature: 0,
+        top_p: 1.0,
+        n: 1,
+        stop: ['\n']
+      })
+    }).then(res => res.json())
+      .then(data => {
+        const translations = data.choices[0].text.trim().split('\n');
+        const translation = translations[translations.length - 1];
+        resolve(translation);
+      })
+      .catch(error => reject(error));
   });
-  
-  const data = await response.json();
-  
-  if (data?.translations?.length > 0) {
-    return data.translations[0].translation;
-  } else {
-    console.error('Translation failed');
-    return label;
-  }
 }
